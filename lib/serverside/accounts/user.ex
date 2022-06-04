c34185e981ce546ec6d3d038dfc3cd519ba9ed5f
cafir <reza.firhan@kargo.tech>
2022-06-04 23:01:30 +0700

    add password hash function

--- lib/serverside/accounts/user.ex
+++ lib/serverside/accounts/user.ex
@@ -27,6 +27,10 @@ defmodule Serverside.Accounts.User do
     |> hash_password
   end
 
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
   defp hash_password(changeset) do
     changeset
   end
--- mix.exs
+++ mix.exs
@@ -48,7 +48,9 @@ defmodule Serverside.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:comeonin, "~> 5.3"},
+      {:argon2_elixir, "~> 2.0"}
     ]
   end
 
